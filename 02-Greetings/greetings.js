95809e8cc98d2f5368e604ba66b41fc8d7402de4
Diego Perez <diegopm2000@gmail.com>
2021-10-02 17:11:58 +0200

    feat: hellowordl remastered & greetings

---
+++ 02-Greetings/greetings.js
@@ -0,0 +1,4 @@
+function greetings(name) {
+    return "Hello, " + name.toUpperCase() + "!";
+}
+console.log(greetings("Diego"));
---
+++ 02-Greetings/greetings.ts
@@ -0,0 +1,5 @@
+function greetings(name: string): string {
+  return `Hello, ${name.toUpperCase()}!`;
+}
+
+console.log(greetings("Diego"))
